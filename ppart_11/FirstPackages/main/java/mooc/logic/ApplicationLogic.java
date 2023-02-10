b9779be6114293d0220d6e58e7eb1fce2b70a985
gitpk-0 <kell108p@protonmail.com>
2023-02-10 09:18:53 -0500

    first packages

---
+++ ppart_11/FirstPackages/main/java/mooc/logic/ApplicationLogic.java
@@ -0,0 +1,23 @@
+package mooc.logic;
+
+import mooc.ui.TextInterface;
+import mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+
+    private UserInterface ui;
+
+    public ApplicationLogic(UserInterface ui) {
+        this.ui = ui;
+    }
+
+    public void execute(int times) {
+        int count = 0;
+        while (count < times) {
+            System.out.println("Application logic is working");
+            ui.update();
+            count++;
+        }
+    }
+
+}
