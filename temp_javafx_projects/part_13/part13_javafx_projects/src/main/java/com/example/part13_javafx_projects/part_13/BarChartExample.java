c09562c81017bb2bb0f29cab688be09dcb23d3b5
gitpk-0 <kell108p@protonmail.com>
2023-02-18 19:10:16 -0500

    bar chart alphabetically

--- temp_javafx_projects/part_13/part13_javafx_projects/src/main/java/com/example/part13_javafx_projects/part_13/BarChartExample.java
+++ temp_javafx_projects/part_13/part13_javafx_projects/src/main/java/com/example/part13_javafx_projects/part_13/BarChartExample.java
@@ -8,6 +8,8 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
+import java.util.Comparator;
+
 public class BarChartExample extends Application {
 
     @Override
@@ -20,13 +22,22 @@ public class BarChartExample extends Application {
         barChart.setLegendVisible(false);
 
         XYChart.Series populations = new XYChart.Series();
+        XYChart.Series sortedPopulations = new XYChart.Series();
         populations.getData().add(new XYChart.Data("Sweden", 10313447));
         populations.getData().add(new XYChart.Data("Denmark", 5809502));
         populations.getData().add(new XYChart.Data("Finland", 5537364));
         populations.getData().add(new XYChart.Data("Norway", 5372191));
         populations.getData().add(new XYChart.Data("Iceland", 343518));
 
-        barChart.getData().add(populations);
+        // sort populations data by country name alphabetically -> add it to sortedPopulations series
+        populations.getData().stream().map(data -> data.toString().substring(5))
+                .map(data -> data.toString().substring(0, data.toString().length() - 6))
+                .sorted()
+                .forEach(obj -> sortedPopulations.getData().add(new XYChart.Data(String.valueOf(obj.toString().substring(0, obj.toString().indexOf(","))), Integer.valueOf(obj.toString().substring(obj.toString().indexOf(",") +1, obj.toString().length() -1)))));
+
+
+
+        barChart.getData().add(sortedPopulations);
         Scene view = new Scene(barChart, 640, 480);
         stage.setScene(view);
         stage.show();
